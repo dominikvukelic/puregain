dd996360c822b346ae6b49995d55dd5e64d6f161
Dominik Vukelic <letotalhavoc@gmail.com>
2022-06-02 15:03:08 +0200

    started designing listofexercises component

--- src/pages/ListOfExercises.js
+++ src/pages/ListOfExercises.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Button, Box, Select, Flex } from '@chakra-ui/react';
+import { Button, Select, Heading } from '@chakra-ui/react';
 
 function ListOfExercises() {
     return (
         <div>
             <Button colorScheme="teal" /* onClick={handleAddExercise} */>Add a new exercise</Button>
-            <Select placeholder="Check existing exercises" paddingTop="40px" bg="teal">
+            <Heading as="h3" size="lg">
+                List of existing exercises
+            </Heading>
+            <Select paddingTop="40px" bg="teal">
                 <option value="option1">Option 1</option>
                 <option value="option2">Option 2</option>
                 <option value="option3">Option 3</option>
